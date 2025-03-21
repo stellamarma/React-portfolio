42de036d05fb7ae41c17eebc6ad8dfd88a4872f7
stellamarma <stellamarmat@gmail.com>
2025-03-21 10:40:44 +0200

    Add api

--- my-portfolio-react/src/App.jsx
+++ my-portfolio-react/src/App.jsx
@@ -2,22 +2,23 @@ import "../src/css/App.css";
 import Home from './pages/Home'
 import {Router,Route, Routes} from "react-router-dom";
 import Favorites from './pages/Favoirites';
+import { MovieProvider } from "./contexts/MovieContext";
 import NavBar from './components/NavBar';
 
 function App() {
 
 
   return (
-  <div>
-    <NavBar />
-  <main className="main-content">
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/favorites" element={<Favorites  />} />
-    </Routes>
-  </main>
-  </div>
-  )
+    <MovieProvider>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
+    </MovieProvider>
+  );
 }
 
 export default App
--- my-portfolio-react/src/components/MovieCard.jsx
+++ my-portfolio-react/src/components/MovieCard.jsx
@@ -1,23 +1,30 @@
-import "../css/MovieCard.css";
+import "../css/MovieCard.css"
+import { useMovieContext } from "../contexts/MovieContext"
 
-function MovieCard({movie}){
+function MovieCard({movie}) {
+    const {isFavorite, addToFavorites, removeFromFavorites} = useMovieContext()
+    const favorite = isFavorite(movie.id)
 
-    function onFavoriteClick(){
-        alert("clicked")
+    function onFavoriteClick(e) {
+        e.preventDefault()
+        if (favorite) removeFromFavorites(movie.id)
+        else addToFavorites(movie)
     }
 
     return <div className="movie-card">
         <div className="movie-poster">
-            <img src={movie.url} alt={movie.title}/>
+            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title}/>
             <div className="movie-overlay">
-                <button class="favourite-btn" onClick={onFavoriteClick}>♡</button>
+                <button className={`favorite-btn ${favorite ? "active" : ""}`} onClick={onFavoriteClick}>
+                    ♥
+                </button>
             </div>
         </div>
-        <div class="movie-info">
+        <div className="movie-info">
             <h3>{movie.title}</h3>
-            <p>{movie.release_date}</p>
+            <p>{movie.release_date?.split("-")[0]}</p>
         </div>
     </div>
-
 }
-export default MovieCard;
+
+export default MovieCard
